713b42d094f746762ad6bbb1ec12ed4347b09eac
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2022-08-23 08:39:20 +0700

    Create charge1000.bat

---
+++ charge1000.bat
@@ -0,0 +1,25 @@
+@echo off
+
+cls
+
+color 2
+
+:: set charge status is charging
+adb shell dumpsys battery set status 0
+adb shell dumpsys battery set status 2
+
+:: change battery from 5% to 1000%
+for /L %%d in (5 1 1000) do (
+echo Charging %%d%%
+adb shell dumpsys battery set level %%d
+ping 127.0.0.1 -n 1 -w 500 > NUL
+)
+
+:: set charge status is full
+adb shell dumpsys battery set status 3
+
+echo ------------------------
+echo ------Finished----------
+echo ------------------------
+
+pause
